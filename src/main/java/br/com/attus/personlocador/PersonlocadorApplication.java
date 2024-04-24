0956a538e02c5c510e50df162d439c5a80512db6
Jefferson Marques <jeffer.son_dasilva@live.com>
2024-04-24 20:34:59 -0300

    feat: initial commit

---
+++ src/main/java/br/com/attus/personlocador/PersonlocadorApplication.java
@@ -0,0 +1,13 @@
+package br.com.attus.personlocador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonlocadorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonlocadorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/attus/personlocador/PersonlocadorApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.attus.personlocador;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonlocadorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
